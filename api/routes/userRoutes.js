fe2df344872eba489ffdeff89baa966a6e021a21
dijiflex <dijiflex@gmail.com>
2021-12-14 15:25:43 +0300

    removed global router protection

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -8,10 +8,12 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 
-//THIS WILL PROCTECT all the routes under it
-router.use(authController.protect);
-
-router.get('/me', userController.getMe, userController.getUser);
-router.patch('/updateMe', userController.updateMe);
+router.get(
+  '/me',
+  authController.protect,
+  userController.getMe,
+  userController.getUser
+);
+router.patch('/updateMe', authController.protect, userController.updateMe);
 
 module.exports = router;
