5bb19957088671c5d9d022804184054c7b0fe1e0
dijiflex <dijiflex@gmail.com>
2021-12-16 17:36:43 +0300

    protected the user route

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import UserPage from '../pages/UserPage';
+import ProtectedRoute from './ProtectedRoute';
 
 const App = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route path="/user" component={UserPage} />
+      <ProtectedRoute path="/user" component={UserPage} />
     </Switch>
   </Router>
 );
