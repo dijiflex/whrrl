849cae771b392d1ab1a79eb5dd00528c9d417014
dijiflex <dijiflex@gmail.com>
2021-12-16 17:36:18 +0300

    implemented protected route

---
+++ client/src/app/ProtectedRoute.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+
+const ProtectedRoute = ({ component: Component, userRole, ...rest }) => {
+  const { currentUser } = useSelector(state => state.userState);
+  const { enqueueSnackbar } = useSnackbar();
+
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (!currentUser) {
+          enqueueSnackbar('Please Login', { variant: 'error' });
+          return (
+            <Redirect
+              to={{
+                pathname: '/',
+                state: {
+                  from: props.location
+                }
+              }}
+            />
+          );
+        }
+        return <Component {...props} />;
+      }}
+    />
+  );
+};
+
+export default ProtectedRoute;
