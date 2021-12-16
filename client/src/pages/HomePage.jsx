b5d805cbcece387b3e2cbace810cdec1e21e1700
dijiflex <dijiflex@gmail.com>
2021-12-16 14:29:42 +0300

    center page items

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,18 +3,19 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import RegistrationForm from '../components/RegistrationComponent/RegistrationForm';
+import RegistrationComponent from '../components/RegistrationComponent/RegistrationComponent';
 
 const HomePage = () => (
 
   <Container maxWidth="lg">
-    <Grid container spacing={2} direction="column">
+    <Grid container spacing={0} direction="column">
       <Grid item xs={12}>
-        <Typography variant="h1" sx={{ mt: 5 }} color="initial">
+        <Typography variant="h1" sx={{ mt: 5 }} color="initial" align="center">
           Welcome to Whrrl
         </Typography>
       </Grid>
-      <Grid item xs={12}>
-        <RegistrationForm />
+      <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+        <RegistrationComponent />
       </Grid>
 
     </Grid>
