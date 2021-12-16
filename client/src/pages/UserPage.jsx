959ff4cc7130611815d2b58ca37a6604f3f1179f
dijiflex <dijiflex@gmail.com>
2021-12-16 13:01:18 +0300

    created the userpage

---
+++ client/src/pages/UserPage.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+
+const UserPage = () => {
+  return (
+    <div>
+      This is
+    </div>
+  );
+};
+
+export default UserPage;
